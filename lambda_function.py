5e615b0064d713bf2d4b8ac1c451ff7eb6c1557b
Gogojab <gogojab@gmail.com>
2017-11-18 20:28:05 +0000

    change to MeetMyGuest

--- lambda_function.py
+++ lambda_function.py
@@ -99,6 +99,15 @@ def set_color_in_session(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+def say_hello_to_x(intent, session):
+    session_attributes = {}
+    card_title = "Said hello to"
+    speech_output = "intent 1"
+    
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
 
 def get_color_from_session(intent, session):
     session_attributes = {}
@@ -150,7 +159,8 @@ def on_intent(intent_request, session):
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
-    
+    if intent_name == "SayHelloToX":
+	return say_hello_to_x(intent, session)
 
     # Dispatch to your skill's intent handlers
     if intent_name == "MyColorIsIntent":
@@ -191,7 +201,9 @@ def lambda_handler(event, context):
     function.
     """
     if (event['session']['application']['applicationId'] !=
-            "amzn1.ask.skill.b6c48e19-be7b-476e-b118-f371cc43fb14"):
+#    b14 is SayHelloToX, 645 is MeetMyGuest
+#            "amzn1.ask.skill.b6c48e19-be7b-476e-b118-f371cc43fb14"):
+            "amzn1.ask.skill.b3bf10c3-db55-412c-a7ce-0490a9b4e645"):
         raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
