d716765b25c015c3d563deb28d780b338ea2ab9f
Gogojab <gogojab@gmail.com>
2017-11-25 17:54:18 +0000

    Add HelpIntent

--- lambda_function.py
+++ lambda_function.py
@@ -62,6 +62,14 @@ def handle_session_end_request():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+def handle_help_request():
+    card_title = "Help Requested"
+    speech_output = "Tell me your friend's name, for example, their name is Bob"
+    session_attributes = {}
+    should_end_session =  False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
 
 def meet_my_friend(intent, session):
     session_attributes = {}
@@ -116,6 +124,8 @@ def on_intent(intent_request, session):
         return handle_session_end_request()
     elif intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
+    elif intent_name == "AMAZON.HelpIntent":
+        return handle_help_request()
     else:
         print("invalid intent name=" + intent_name)
         raise ValueError("Invalid intent")
