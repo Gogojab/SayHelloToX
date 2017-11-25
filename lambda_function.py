4789342623adf8b43061ef8e5528f6306b369a63
Gogojab <gogojab@gmail.com>
2017-11-25 10:18:41 +0000

    update code card to say MeetMyFriend rather than SessionSpeechlet

--- lambda_function.py
+++ lambda_function.py
@@ -20,8 +20,8 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': "MeetMyFriend - " + title,
+            'content': "MeetMyFriend - " + output
         },
         'reprompt': {
             'outputSpeech': {
